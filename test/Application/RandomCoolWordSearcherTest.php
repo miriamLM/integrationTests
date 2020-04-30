2fa08e36838ffd4fc45b1f9786ea69c1e60cb7f6
JesusCH92 <jcocaharo@hotmail.com>
2020-04-30 22:18:38 +0200

    create the first test for RandomCoolWordSearcher use case which check if what is returned is what you expected

--- test/Application/RandomCoolWordSearcherTest.php
+++ test/Application/RandomCoolWordSearcherTest.php
@@ -1,12 +1,27 @@
-<?php
+<?php 
 
 declare(strict_types=1);
 
 namespace LaSalle\ChupiTest\Application;
 
 use PHPUnit\Framework\TestCase;
+use LaSalle\ChupiTest\Infrastructure\InMemoryCoolWordStub;
+use LaSalle\ChupiProject\Module\CoolWord\Domain\RandomCoolWordSearcher;
 
 final class RandomCoolWordSearcherTest extends TestCase
 {
+    /**
+     * @test
+     */
+    public function shouldGetRandomCoolWord()
+    {
+        $coolWordStub = new InMemoryCoolWordStub();
+        $randomColorSearcher = new RandomCoolWordSearcher($coolWordStub);
+
+        $coolWordReceive = $randomColorSearcher();
+
+        $this->assertEquals('Chachi pistachi!', $coolWordReceive);
+
+    }
 
 }
