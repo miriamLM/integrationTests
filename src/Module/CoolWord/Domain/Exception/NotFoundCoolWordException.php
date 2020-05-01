f32c4403a48c3413f510a10744e9ce1c2e71a434
JesusCH92 <jcocaharo@hotmail.com>
2020-05-01 17:54:26 +0200

    add exception-> this exception was moved from Color module

---
+++ src/Module/CoolWord/Domain/Exception/NotFoundCoolWordException.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace LaSalle\ChupiProject\Module\CoolWord\Domain\Exception;
+
+use DomainException;
+
+final class NotFoundCoolWordException extends DomainException
+{
+    public function __construct()
+    {
+        parent::__construct($this->errorMessage());
+    }
+
+    public function errorMessage():string
+    {
+        return "Not found cool word";
+    }
+}
