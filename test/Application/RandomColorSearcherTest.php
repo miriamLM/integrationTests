1e126e893cd3498964e211695bae28182eebed77
JesusCH92 <jcocaharo@hotmail.com>
2020-05-01 18:42:07 +0200

    create test class for RandomColorSearcher case use

---
+++ test/Application/RandomColorSearcherTest.php
@@ -0,0 +1,15 @@
+<?php 
+
+declare(strict_types=1);
+
+namespace LaSalle\ChupiTest\Application;
+
+use PHPUnit\Framework\TestCase;
+
+final class RandomColorSearcherTest extends TestCase
+{
+    public function shouldGetRandomColor()
+    {
+        
+    }
+}
