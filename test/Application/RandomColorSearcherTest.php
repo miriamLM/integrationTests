fd35e6523a226a30f99c7677488594197032a11e
JesusCH92 <jcocaharo@hotmail.com>
2020-05-01 19:03:58 +0200

    added a test for check if color received is the color expected

--- test/Application/RandomColorSearcherTest.php
+++ test/Application/RandomColorSearcherTest.php
@@ -4,12 +4,23 @@ declare(strict_types=1);
 
 namespace LaSalle\ChupiTest\Application;
 
+use LaSalle\ChupiProject\Module\Color\Domain\RandomColorSearcher;
+use LaSalle\ChupiTest\Infrastructure\InMemoryColorStub;
 use PHPUnit\Framework\TestCase;
 
 final class RandomColorSearcherTest extends TestCase
 {
+    /**
+     * @test
+     */
     public function shouldGetRandomColor()
     {
-        
+        $colorStub = new InMemoryColorStub();
+        $randomColorSearcher = new RandomColorSearcher($colorStub);
+
+        $colorReceive = $randomColorSearcher();
+
+        $this->assertEquals('green', $colorReceive);
+
     }
 }
