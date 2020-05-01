d02721600b7cddb099ca745a212ece50eda20138
JesusCH92 <jcocaharo@hotmail.com>
2020-05-01 19:16:52 +0200

    create a test for check if throw exception when color collection is empty

--- test/Application/RandomColorSearcherTest.php
+++ test/Application/RandomColorSearcherTest.php
@@ -4,8 +4,9 @@ declare(strict_types=1);
 
 namespace LaSalle\ChupiTest\Application;
 
+use LaSalle\ChupiProject\Module\Color\Domain\Exception\NotFoundColorException;
 use LaSalle\ChupiProject\Module\Color\Domain\RandomColorSearcher;
-use LaSalle\ChupiTest\Infrastructure\InMemoryColorStub;
+use LaSalle\ChupiTest\Infrastructure\{InMemoryColorStub, InMemoryEmptyColorStub};
 use PHPUnit\Framework\TestCase;
 
 final class RandomColorSearcherTest extends TestCase
@@ -21,6 +22,22 @@ final class RandomColorSearcherTest extends TestCase
         $colorReceive = $randomColorSearcher();
 
         $this->assertEquals('green', $colorReceive);
+
+    }
+
+    /**
+     * @test
+     */
+    public function shouldTrowExceptionWhenCoolWordCollectionIsEmpty()
+    {
+        $this->expectException(NotFoundColorException::class);
+
+        $colorStub = new InMemoryEmptyColorStub();
+        $randomColorSearcher = new RandomColorSearcher($colorStub);
+
+        $colorReceive = $randomColorSearcher();
+
+        $this->assertEquals([], $colorReceive);
 
     }
 }
