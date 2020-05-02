6543a3a695c0bcf5eaaa58926088d0bae9e355fc
Miriam Lopez <miriam.lm@students.salle.url.edu>
2020-05-02 23:15:16 +0200

    create shouldCheckIfCreateDifferentRandomColor test for check if _random_color_except generate a different color of the entry color

--- test/Application/CoolWordWithStyleCreatorTest.php
+++ test/Application/CoolWordWithStyleCreatorTest.php
@@ -6,7 +6,9 @@ namespace LaSalle\ChupiTest\Application;
 
 use LaSalle\ChupiProject\Module\Color\Domain\RandomColorSearcher;
 use LaSalle\ChupiProject\Module\Color\Infrastructure\InMemoryColorRepository;
+use LaSalle\ChupiTest\Domain\ColorExpectFakeRepository;
 use LaSalle\ChupiTest\Infrastructure\Fake\RandomColorExceptFake;
+use Mockery;
 use PHPUnit\Framework\TestCase;
 
 final class CoolWordWithStyleCreatorTest extends TestCase
@@ -24,6 +26,28 @@ final class CoolWordWithStyleCreatorTest extends TestCase
         $actualColor = $randomColorExceptFake->_random_color_except("green", $randomColorSearcher);
 
         $this->assertNotEquals("green", $actualColor);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldCheckIfCreateDifferentRandomColor()
+    {
+
+        $colorSearcher = new RandomColorSearcher(new InMemoryColorRepository());
+
+        $bgColor = $colorSearcher();
+
+        $colorExpectFakeRepository = Mockery::mock(ColorExpectFakeRepository::class);
+        $colorExpectFakeRepository
+            ->shouldReceive('_random_color_except')
+            ->once()
+            ->with($bgColor,\Hamcrest\Matchers::equalTo($colorSearcher));
+
+        $fgColor = $colorExpectFakeRepository->_random_color_except($bgColor, $colorSearcher);
+
+        $this->assertNotEquals($bgColor, $fgColor);
+
     }
 }
 
