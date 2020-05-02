92ff5d287b6dad1f7468e56da3abc63e22528b5e
Miriam Lopez <miriam.lm@students.salle.url.edu>
2020-05-02 23:13:28 +0200

    add implementation of ColorExceptFakeRepository for _random_color_except method

--- test/Infrastructure/Fake/RandomColorExceptFake.php
+++ test/Infrastructure/Fake/RandomColorExceptFake.php
@@ -5,8 +5,9 @@ declare(strict_types=1);
 namespace LaSalle\ChupiTest\Infrastructure\Fake;
 
 use LaSalle\ChupiProject\Module\Color\Domain\RandomColorSearcher;
+use LaSalle\ChupiTest\Domain\ColorExpectFakeRepository;
 
-final class RandomColorExceptFake
+final class RandomColorExceptFake implements ColorExpectFakeRepository
 {
     public function _random_color_except(string $colorExcept, RandomColorSearcher $randomColorSearcher):string
     {
