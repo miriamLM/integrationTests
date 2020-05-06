dde1b28c76a1338a4936c268efdf8e0848b2f228
JesusCH92 <jcocaharo@hotmail.com>
2020-05-06 22:22:59 +0200

    delete shouldCheckIfCreateDifferentRandomColor test because we don't need it (with shouldCreateDifferentRandomColor is enough)

--- test/Application/CoolWordWithStyleCreatorTest.php
+++ test/Application/CoolWordWithStyleCreatorTest.php
@@ -6,9 +6,7 @@ namespace LaSalle\ChupiTest\Application;
 
 use LaSalle\ChupiProject\Module\Color\Domain\RandomColorSearcher;
 use LaSalle\ChupiProject\Module\Color\Infrastructure\InMemoryColorRepository;
-use LaSalle\ChupiTest\Domain\ColorExpectFakeRepository;
 use LaSalle\ChupiTest\Infrastructure\Fake\RandomColorExceptFake;
-use Mockery;
 use PHPUnit\Framework\TestCase;
 
 final class CoolWordWithStyleCreatorTest extends TestCase
@@ -28,26 +26,5 @@ final class CoolWordWithStyleCreatorTest extends TestCase
         $this->assertNotEquals("green", $actualColor);
     }
 
-    /**
-     * @test
-     */
-    public function shouldCheckIfCreateDifferentRandomColor()
-    {
-
-        $colorSearcher = new RandomColorSearcher(new InMemoryColorRepository());
-
-        $bgColor = $colorSearcher();
-
-        $colorExpectFakeRepository = Mockery::mock(ColorExpectFakeRepository::class);
-        $colorExpectFakeRepository
-            ->shouldReceive('_random_color_except')
-            ->once()
-            ->with($bgColor,\Hamcrest\Matchers::equalTo($colorSearcher));
-
-        $fgColor = $colorExpectFakeRepository->_random_color_except($bgColor, $colorSearcher);
-
-        $this->assertNotEquals($bgColor, $fgColor);
-
-    }
 }
 
